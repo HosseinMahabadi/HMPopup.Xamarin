c8ba56856a33757a55f5c62a0eac28dfe3646745
Hossein Mahabadi <hossein.mahabadi@hotmail.com>
2025-01-01 11:14:50 +0330

    2.0.90

--- HMPopup/HMPopup/MauiProgram.cs
+++ HMPopup/HMPopup/MauiProgram.cs
@@ -2,9 +2,9 @@ using HMExtension.Maui;
 using Microsoft.Maui.Hosting;
 using System.Reflection;
 
-[assembly: ExportFont("Samim.ttf", Alias = "samim")]
+/*[assembly: ExportFont("Samim.ttf", Alias = "samim")]
 [assembly: ExportFont("Samim-Medium.ttf", Alias = "samimMedium")]
-[assembly: ExportFont("Samim-Bold.ttf", Alias = "samimBold")]
+[assembly: ExportFont("Samim-Bold.ttf", Alias = "samimBold")]*/
 
 namespace HMPopup;
 
@@ -12,16 +12,16 @@ public static class MauiProgram
 {
     public static MauiAppBuilder UseHMPopup(this MauiAppBuilder builder)
     {
-        Assembly assembly = Assembly.GetCallingAssembly();
-        
+        //Assembly assembly = Assembly.GetCallingAssembly();
+
         builder
-            .UseHMExtension()
-            .ConfigureFonts (fonts =>
+            .UseHMExtension();
+            /*.ConfigureFonts (fonts =>
             {
                 fonts.AddEmbeddedResourceFont(assembly, "Samim.ttf", "samim");
                 fonts.AddEmbeddedResourceFont(assembly, "Samim-Medium.ttf", "samimMedium");
                 fonts.AddEmbeddedResourceFont(assembly, "Samim-Bold.ttf", "samimBold");
-            });
+            });*/
 
         return builder;
     }
